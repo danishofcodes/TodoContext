ea4221320d57ae590062eb9009fc871b78386eb0
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-10-05 16:59:55 +0530

    Added functionality for edit and save  and delete todos also added line-through and todo item bg-color change
     Added functionality for edit and save  and delete todos also added line-through and todo item bg-color change

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,36 +1,58 @@
 import { useState } from "react";
-import {useTodo} from "../contexts/ToDoContext"
+import { useTodo } from "../contexts/ToDoContext"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClose, faDumpster, faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
-export default function TodoItem({todo}) {
-const [isEditable, setIsEditable] =  useState(false);
-  const {updateTodo, deleteTodo, toggleComplete} = useTodo();
+import { faClose, faDumpster, faPencil, faSave, faTrash } from "@fortawesome/free-solid-svg-icons";
+export default function TodoItem({ todo }) {
+  const [isEditable, setIsEditable] = useState(false);
+  const { updateTodo, deleteTodo, toggleComplete } = useTodo();
   const [todoMessage, setTodoMessage] = useState(todo.todo)
 
 
-  const edit = ()=>{
-    updateTodo(todo.id, {...todo, todo:todoMessage});
-    setIsEditable(false)
+  const edit_todo = () => {
+    updateTodo(todo.id, { ...todo, todo: todoMessage });
+    setIsEditable(false);
+    console.log("can edit")
   }
 
-  const toggle = ()=>{
+  const toggler = () => {
     toggleComplete(todo.id)
   }
 
-  // const deleteTodo = ()=>{
-  //   updateTodo(todo.id, {...todo, todo:todoMessage});
-  //   setIsEditable(false)
+  const delete_todo = () => {
+    deleteTodo(todo.id);
+  }
+
+  // const save_todo = ()=>{
+  //   setIsEditable(true)
   // }
+
+
   return (
-    <div class="flex items-center justify-between p-4 bg-[#172842] border border-gray-200 rounded-lg mb-2">
-    <div class="flex items-center">
-        <input type="checkbox" class="mr-2" />
-        <span class={`${toggleComplete && "line-through" }text-gray-100`}>{todo.todo}</span>
+    <div class={`flex items-center justify-between p-4  border border-gray-200 rounded-lg mb-2 ${todo.completed ? "bg-[#3f3f3f]" : "bg-[#172842]"}`} id={todo.id}>
+      <div class="flex items-center grow">
+        <input type="checkbox" class="mr-2" checked={todo.completed} onChange={toggler} disabled={isEditable}/>
+
+        <input type="text"
+          onChange={(e) => setTodoMessage(e.target.value)}
+          className={` w-full mr-3 bg-transparent ${isEditable ? "bg-white text-gray-800 p-2" : ""} ${todo.completed ? "line-through text-gray-400" : "undefined text-gray-100"}`} value={todoMessage} readOnly={!isEditable} />
+      </div>
+
+      <div className="flex ">
+        <button
+          onClick={() => {
+            if (todo.completed) return;
+            if (isEditable) {
+              edit_todo(); 
+            }
+            setIsEditable(!isEditable); 
+          }}
+          className={`hover:underline mr-2 px-3 text-gray-50 rounded-full ${todo.completed ? "bg-slate-500 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-400 cursor-pointer"}`}
+          disabled={todo.completed}
+        >
+          {isEditable ? <FontAwesomeIcon icon={faSave} /> : <FontAwesomeIcon icon={faPencil} />}
+        </button>
+        <button onClick={delete_todo} class="hover:underline   px-3 text-gray-50 rounded-full bg-red-500 hover:bg-red-400 cursor-pointer" ><FontAwesomeIcon icon={faTrash} /></button>
+      </div>
     </div>
-    <div className="flex ">
-        <button class="hover:underline mr-2 bg-blue-500 px-3 text-gray-50 rounded-full "><FontAwesomeIcon icon={faPencil}/></button>
-        <button class=" hover:underline  bg-red-500 px-3 text-gray-50 rounded-full" ><FontAwesomeIcon icon={faTrash}/></button>
-    </div>
-</div>
   )
 }
